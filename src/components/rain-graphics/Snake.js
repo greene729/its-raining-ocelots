7321012e19b34f88a1df63c031965631144c7a14
maxmraz <max.mraz.11@gmail.com>
2018-11-16 11:58:33 -0500

    add animal rain

---
+++ src/components/rain-graphics/Snake.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import snake from '../../images/snake.png'
+
+const Snake = () => {
+
+	return (
+		<img className="rain-graphic" src={snake} alt="ssssssssssssssss." />
+	)
+}
+
+export default Snake
