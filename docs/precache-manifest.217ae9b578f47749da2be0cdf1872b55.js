83e40006684fda00b3f2bce05acbfc9321f57bf5
Alexander Greene <greene.alexanderj@gmail.com>
2019-01-02 11:47:03 -0500

    yarn build with docs folder for github deployment

---
+++ docs/precache-manifest.217ae9b578f47749da2be0cdf1872b55.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "269d352bf296d3c059b96d47fca01ae5",
+    "url": "/its-raining-ocelots/static/media/tiger.269d352b.png"
+  },
+  {
+    "revision": "e851ea7221dcaaf905e1",
+    "url": "/its-raining-ocelots/static/css/main.69472c7d.chunk.css"
+  },
+  {
+    "revision": "cf7abf045c7a2ff306a8",
+    "url": "/its-raining-ocelots/static/js/1.cf7abf04.chunk.js"
+  },
+  {
+    "revision": "4be4eabc79eca1365c7e",
+    "url": "/its-raining-ocelots/static/js/runtime~main.4be4eabc.js"
+  },
+  {
+    "revision": "fa029af8e38a6c7435c82b0ac5ef3899",
+    "url": "/its-raining-ocelots/static/media/ghost.fa029af8.png"
+  },
+  {
+    "revision": "e851ea7221dcaaf905e1",
+    "url": "/its-raining-ocelots/static/js/main.e851ea72.chunk.js"
+  },
+  {
+    "revision": "218c2688a28e2b70bd5323d6ef41b71a",
+    "url": "/its-raining-ocelots/static/media/bison.218c2688.png"
+  },
+  {
+    "revision": "e3c618a5c89ffbb044ff8f9abe443f3c",
+    "url": "/its-raining-ocelots/static/media/snake.e3c618a5.png"
+  },
+  {
+    "revision": "0dd1e5e35f2c395a8166708b726a6df0",
+    "url": "/its-raining-ocelots/static/media/algore.0dd1e5e3.png"
+  },
+  {
+    "revision": "563567b5dfe1acd56d546741cea0f827",
+    "url": "/its-raining-ocelots/static/media/ocedrop.563567b5.png"
+  },
+  {
+    "revision": "a28036ca3e3cdd68967650b2ecc354c1",
+    "url": "/its-raining-ocelots/index.html"
+  }
+];
